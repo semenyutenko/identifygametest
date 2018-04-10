cf5d1e9051df1c6411f958b5bfba3ddd4da5fce9
Andrey <selemenyutenko@gmail.com>
2018-04-11 02:25:36 +0300

    1st_logic

--- app/src/main/java/satedshark/com/vk/identifygametest/MainActivity.java
+++ app/src/main/java/satedshark/com/vk/identifygametest/MainActivity.java
@@ -3,18 +3,31 @@ package satedshark.com.vk.identifygametest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
     static int numCount = 5;
-    static int numVariation = 5;
+    static int numVariation = 4;
+    static int numTips = 2;
+    TextView winview;
+    TextView winViewMess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.textView1);
+        winViewMess = findViewById(R.id.textWin);
+        LinearLayout linearLayout = findViewById(R.id.liner_layout);
+        for (int i = 0; i < linearLayout.getChildCount(); i++){
+            TextView tv = (TextView)linearLayout.getChildAt(i);
+            tv.setText(generator(numCount, numVariation));
+        }
+        winview = (TextView)linearLayout.getChildAt((int)(Math.random() *
+                (linearLayout.getChildCount() - numTips) + numTips));
+        for (int i = 0; i < numTips; i++){
+            onClick(linearLayout.getChildAt(i));
+        }
     }
     private static String generator(int n, int m){
         StringBuilder a = new StringBuilder();
@@ -27,6 +40,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClick(View view) {
         TextView tv = (TextView)view;
-        tv.setText(generator(numCount, numVariation));
+        tv.setClickable(false);
+        tv.setTextColor(getResources().getColor(R.color.colorCh));
+        if (tv == winview) {
+            winViewMess.setVisibility(View.VISIBLE);
+        } else {
+            tv.setText(tv.getText() + "    " + coun((String)tv.getText(), (String) winview.getText()).toString());
+        }
     }
+
+    private static Integer coun(String z, String y){
+        Integer sum = 0;
+        for(int i = 0; i < z.length(); i++){
+            if(z.charAt(i) == y.charAt(i)){
+                sum++;
+            }
+        }
+            return sum;
+        }
 }
